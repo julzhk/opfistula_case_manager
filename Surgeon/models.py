097f4d5dd844d965bbd028c4fd4254b4fc544fb1
Julian Harley <julian.harley@gmail.com>
2014-09-27 16:11:24 +0100

    add Surgeon

---
+++ Surgeon/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from django.contrib.auth.models import User
+
+
+class Surgeon(models.Model):
+    class Meta:
+        verbose_name = 'Surgeon'
+
+    user = models.OneToOneField(User)
+    institution = models.CharField(verbose_name='Institution',
+                                   max_length=settings.DEFAULT_LONG_CHARFIELD_LENGTH)
---
+++ Surgeon/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django import forms
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from CaseEntry.models import PatientRecord, PatientRecordForm, Case
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from Surgeon.models import Surgeon
+
+
+@login_required
+def surgeon_home(request):
+    # a superuser created in python can have no 1-1 Surgeon associated
+    if request.user.is_authenticated():
+        this_user = request.user
+        try:
+            cases = this_user.surgeon.case_set.all()
+        except Surgeon.DoesNotExist:
+            this_user.surgeon = Surgeon()
+            this_user.surgeon.save()
+            this_user.save()
+            cases = this_user.surgeon.case_set.all()
+        return render(request,
+                      'surgeon_home.html',
+                      {
+                          'cases': cases,
+                          'Surgeon': this_user
+                      })
+    else:
+        return HttpResponse('not known')
+
+
+def home(request):
+    return render(request,
+                  'home.html',
+        {})
