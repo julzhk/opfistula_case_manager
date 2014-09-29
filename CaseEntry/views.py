fd96f3d38e4efa98dab0448a9d5fef8083e99e01
julzhk <jharley@vam.ac.uk>
2014-09-29 18:48:36 +0100

    add comment form to case view

--- CaseEntry/views.py
+++ CaseEntry/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django import forms
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
-from CaseEntry.models import PatientRecord, PatientRecordForm, Case, PatientRecordReadOnlyForm
 from django.contrib.auth.decorators import login_required
-
+from django.forms import ModelForm
+from CaseEntry.models import PatientRecord, PatientRecordForm, Case, PatientRecordReadOnlyForm
+from CaseNotes.models import Note
 
 @login_required
 def case_form(request, id=None):
@@ -36,11 +37,18 @@ def casesubmitted(request):
     return render(request, 'casesubmitted.html')
 
 
+class NoteForm(ModelForm):
+    class Meta:
+        model = Note
+        fields = ['message', ]
+
 def view_case(request, id):
     try:
         id = int(id)
         case = Case.objects.get(id=id)
-        return render(request, 'case.html', {'case': case})
+        noteform= NoteForm()
+        return render(request, 'case.html', {'case': case,
+                                             'noteform':noteform})
     except Case.DoesNotExist:
         return HttpResponse('case not accessible')
 
