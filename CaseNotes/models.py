dde4dda44c1159ba47da4d0db2a7b055247ec3a6
jghyllebert <jghyllebert@ nuntra.com>
2014-10-27 21:30:16 +0000

    fixed problems

--- CaseNotes/models.py
+++ CaseNotes/models.py
@@ -7,10 +7,5 @@ from CaseEntry.models import Case
 
 class Note(TimeStampedModel):
     message = models.TextField()
-<<<<<<< HEAD
     case = models.ForeignKey(Case, null=True)
     commenter = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
-=======
-    case = models.ForeignKey(Case,null=True)
-    commenter = models.ForeignKey(User, null=True, blank=True)
->>>>>>> master
