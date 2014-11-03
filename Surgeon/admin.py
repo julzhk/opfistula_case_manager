f747c20e5affd3154d4d8ec19bc027f4b5ad56c4
jghyllebert <jghyllebert@ nuntra.com>
2014-11-03 23:04:09 +0000

    Added custom user model from #18 Removed cdn files #5

--- Surgeon/admin.py
+++ Surgeon/admin.py
@@ -1,19 +1,4 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
+from .models import Surgeon
 
-from Surgeon.models import Surgeon
-
-
-class SurgeonInline(admin.StackedInline):
-    model = Surgeon
-    can_delete = False
-    verbose_name_plural = 'Surgeon'
-
-
-class UserAdmin(UserAdmin):
-    inlines = (SurgeonInline, )
-
-
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
+admin.site.register(Surgeon)
--- Surgeon/models.py
+++ Surgeon/models.py
@@ -1,27 +1,77 @@
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
-from Core.models import TimeStampedModel
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
+        now = timezone.now()
+        email = self.normalize_email(email)
+        admin = True if is_staff or is_superuser else None
+        user = self.model(email=email, is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser, is_admin=admin, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password, **extra_fields):
+        return self._create_user(email, password, False, False,
+                                 **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True,
+                                 **extra_fields)
 
 
-class Surgeon(TimeStampedModel):
-    class Meta:
-        verbose_name = 'Surgeon'
-
-    def __unicode__(self):
-        try:
-            return str(self.user)
-        except AttributeError:
-            return 'none'
-
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+class Surgeon(AbstractBaseUser):
+    """
+    Every user is a Surgeon per default, superusers can do more stuff
+    """
+    #TODO specify fields
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
     institution = models.CharField(verbose_name='Institution',
                                    blank=True,
                                    max_length=settings.DEFAULT_LONG_CHARFIELD_LENGTH)
 
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'Surgeon'
+
+    def __unicode__(self):
+        return self.get_full_name()
+
+    def get_full_name(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+    def get_short_name(self):
+        return "{}. {}".format(self.first_name[0], self.last_name)
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
     def type(self):
-        if self.user.is_superuser:
+        if self.is_superuser:
             return 'Administrator'
-        elif self.user.is_staff:
+        elif self.is_staff:
             return 'Surgeon'
         return 'Non-user'
